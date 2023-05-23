8df4ce3526e9c4c20b915f7a932ee0d090851533
Phylis Mercy Njoroge <110587824+PhylisMercy@users.noreply.github.com>
2023-05-23 10:07:30 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,37 +1,6 @@
-package { 'nginx':
-ensure => installed,
-}
+# Install Nginx web server with puppet.
 
-# Executes a kill on a background/active program
-exec { 'killmenow':
-command   => '/bin/sed -i "/^\s*server_name _*/a add_header X-Served-By \$hostname;" /etc/nginx/sites-enabled/default',
-path      => '/bin',
-subscribe => Package['nginx'],
+exec { 'Install_Nginx':
+  command  =>'apt-get -y update && apt-get -y install nginx && echo "Holberton School Server 2" > /var/www/html/index.nginx-debian.html && new_string="server_name _;\n\tadd_header X-Served-By $HOSTNAME;" && sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default && service nginx restart',
+  provider => 'shell',
 }
-file { '/etc/nginx/sites-enabled/default':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['nginx'],
-    subscribe   => Exec['killmenow'],
-  }
-
-service { 'nginx':
-ensure  => running,
-enable  => true,
-require => File['/etc/nginx/sites-enabled/default'],
-}
-
-exec { 'reload nginx':
-command     => '/etc/init.d/nginx reload',
-refreshonly => true,
-subscribe   => File['/etc/nginx/sites-enabled/default'],
-}
-
-exec { 'restart nginx':
-command     => '/etc/init.d/nginx restart',
-refreshonly => true,
-subscribe   => File['/etc/nginx/sites-enabled/default'],
-}
-
