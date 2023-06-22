c49deadf42dc730d95d3eed5ed18d2254645079e
Phylis Mercy Njoroge <110587824+PhylisMercy@users.noreply.github.com>
2023-06-22 19:57:37 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,18 @@
-# Fix the nginx requests limit
-exec { 'upgrade':
-  path    => '/bin/',
-  command => 'sed -i "s/15/4096" /etc/default/nginx',
+# Increases the Limit of File Descriptors.
+exec { 'mod_nginx_config':
+  command => 'sed -i s/^ULIMIT.*/ULIMIT="\"-n 7000\""/g /etc/default/nginx',
+  onlyif  => 'test -e /etc/default/nginx',
+  path    => ['/bin', '/usr/bin'],
+  notify  => Service['nginx'],
+  require => Package['nginx'],
+  before  => Service['nginx'],
 }
 
-exec { 'restart':
-  path    => '/usr/bin/',
-  command => 'service nginx restart',
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
